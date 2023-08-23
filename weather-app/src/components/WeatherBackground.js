a778a62652e11a32763ed64d737ad1f5c8848caf
connorabernethy <connorabernethy@icloud.com>
2023-08-23 15:25:00 -0700

    Added weather API functionality

--- weather-app/src/components/WeatherBackground.js
+++ weather-app/src/components/WeatherBackground.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const WeatherBackground = (props) => {
-    const { weather } = props;
+
+    const key = process.env.REACT_APP_WEATHER_API_KEY;
+
+    const [weather, setWeather] = useState();
+
+    useEffect(() => {
+        fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=Phoenix`)
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw response;
+        })
+        .then(data => {
+            setWeather(data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, [])
+
+
     return(
         <div className="clear-skies">
-            {props.children}
+            <button onClick={() => {console.log(weather)}}>TEST</button>
+            <div>Current Temperature in Farenheit: {weather['current']['temp_f']}</div>
         </div>
     )
 }
