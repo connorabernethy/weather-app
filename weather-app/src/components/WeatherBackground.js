10909c40a9215c2a1e2c83f5cc664e3575ccee48
connorabernethy <connorabernethy@icloud.com>
2023-08-25 15:19:25 -0700

    Initital change location input added

--- weather-app/src/components/WeatherBackground.js
+++ weather-app/src/components/WeatherBackground.js
@@ -26,7 +26,8 @@ const WeatherBackground = (props) => {
             })
     }, [])
 
-
+    const button = document.getElementById('change-location');
+    const locationInput = document.getElementById('location-input');
     return (
         <Card maxW='sm' w='xl' maxH='lg' h='lg' align='center' justifyContent='center' textAlign='center'>
             <CardBody>
@@ -42,19 +43,21 @@ const WeatherBackground = (props) => {
                             alt='Sunny'
                         />
                         <Text fontSize='4xl'>{weather.current.temp_f}&deg;F</Text>
+                        <Text>{weather.current.condition.text}</Text>
                         </Stack>
                     </div>
                 ) : (
                     <Spinner/>
                 )}
             </CardBody>
-            <Divider />
-            <CardFooter>
-                <ButtonGroup spacing='2'>
-                    <Button variant='solid' colorScheme='blue'>
+            <CardFooter align='center' justifyContent='center' flexDir='column'>
+                    <Button id="change-location" variant='solid' colorScheme='blue' onClick={() => {
+                        locationInput.classList.add('location-input-active');
+                        button.classList.add('move-up');
+                    }}>
                         Change Location
                     </Button>
-                </ButtonGroup>
+                    <input id="location-input" className='location-input' placeholder='Input new location...'></input>
             </CardFooter>
         </Card>
     )
