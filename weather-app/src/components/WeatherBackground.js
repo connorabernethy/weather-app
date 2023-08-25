226c1e138b2466bbaeb7704501c7e302de553732
connorabernethy <connorabernethy@icloud.com>
2023-08-25 15:04:05 -0700

    Added ChakraUI and updated weather rendering

--- weather-app/src/components/WeatherBackground.js
+++ weather-app/src/components/WeatherBackground.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Button, ButtonGroup, Card, CardHeader, CardBody, CardFooter, Divider, Heading, Stack, Text } from '@chakra-ui/react'
+import { Spinner } from '@chakra-ui/react';
+
 
 const WeatherBackground = (props) => {
 
@@ -8,27 +11,52 @@ const WeatherBackground = (props) => {
 
     useEffect(() => {
         fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=Phoenix`)
-        .then(response => {
-            if (response.ok) {
-                return response.json()
-            }
-            throw response;
-        })
-        .then(data => {
-            setWeather(data);
-        })
-        .catch(error => {
-            console.log(error);
-            setWeather('undefined');
-        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw response;
+            })
+            .then(data => {
+                setWeather(data);
+            })
+            .catch(error => {
+                console.log(error);
+                setWeather('undefined');
+            })
     }, [])
 
 
-    return(
-        <div className="">
-            <button onClick={() => {console.log(weather)}}>TEST</button>
-            {weather ? (<div>Current Temp: {weather['current']['temp_f']}</div>) : <div>Weather undefined.</div>}
-        </div>
+    return (
+        <Card maxW='sm' w='xl' maxH='lg' h='lg' align='center' justifyContent='center' textAlign='center'>
+            <CardBody>
+                {weather ? (
+                    <div>
+                        <Stack mt='6' spacing='3' align='center'>
+                            <Heading size='md'>Current Weather</Heading>
+                            <h1>{weather.location.name}, {weather.location.region}</h1>
+                            <img
+                            src={weather.current.condition.icon}
+                            height='50px'
+                            width='50px'
+                            alt='Sunny'
+                        />
+                        <Text fontSize='4xl'>{weather.current.temp_f}&deg;F</Text>
+                        </Stack>
+                    </div>
+                ) : (
+                    <Spinner/>
+                )}
+            </CardBody>
+            <Divider />
+            <CardFooter>
+                <ButtonGroup spacing='2'>
+                    <Button variant='solid' colorScheme='blue'>
+                        Change Location
+                    </Button>
+                </ButtonGroup>
+            </CardFooter>
+        </Card>
     )
 }
 
--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {ChakraProvider} from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className="outer-cloud">
-    <div className='cloud'></div>
-    </div>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
